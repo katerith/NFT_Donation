1e064ab8f1f2787df36dd0e173f7dbc00ba494aa
katerith <thanouaikaterini@gmail.com>
2022-01-24 16:37:24 +0200

    deploy on rinkeby

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
-  export const donateAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
-  export const nftAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
+  export const donateAddress = '0x273580805f41bB4a6a0c3a6B1d8aCe325b782059'
+  export const nftAddress = '0x4Cff9837e13ecF044F7575fe57f20837c944B031'
